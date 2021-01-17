733bfc0a4fc4649cdd6465614cc2b0e86f2d9233
omerronen48 <omerronen123@gmail.com>
2021-01-16 23:25:40 -0800

    idek

--- install.sh
+++ install.sh
@@ -2,22 +2,32 @@
 
 echo "Installing HomeBrew..."
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo "Installing OhMyZSH..."
-sleep 3
+
+echo "Installing Vim, ZSH, & OhMyZSH..."
+sleep 1
 brew install zsh
-sleep 3
+sleep 1
 brew install vim
+sleep 1
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sleep 1
+
+
 echo "Installing PL10K..."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-sleep 3
+sleep 1
+
 echo "Copying zshrc..."
-cat zshRC.txt >> ~/.zshrc
+cat aliases.txt > ~/.zshrc
+
+echo "Installing pathogen..."
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 echo "Copying vimrc..."
 cat VimRC.txt >> ~/.vimrc
-sleep 3
+sleep 1
+
 echo "Installing vim plugins..."
 cd ~/.vim/bundle
 git clone https://github.com/morhetz/gruvbox
