a4bd263416c2ada0f44ff266db5f84b172799da6
omerronen48 <omerronen123@gmail.com>
2021-01-16 21:36:29 -0800

    install script

---
+++ install.sh
@@ -0,0 +1,24 @@
+echo "Installing HomeBrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "Installing OhMyZSH..."
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Installing PL10K..."
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "Copying zshrc..."
+cat zshRC.txt >> ~/.zshrc
+brew install vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo "Copying vimrc..."
+cat VimRC.txt >> ~/.vimrc
+echo "Installing vim plugins..."
+cd ~/.vim/bundle
+git clone https://github.com/morhetz/gruvbox
+git clone https://github.com/shougo/deoplete.nvim
+git clone https://github.com/valloric/youcompleteme
+git clone https://github.com/tpope/vim-unimpaired
+git clone https://github.com/ap/vim-css-color
+git clone https://github.com/raimondi/delimitmate
+git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/vim-scripts/javacomplete
+p10k configure
