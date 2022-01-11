8bcd0f253227ec8042ae02c5b6ffc8711b35070b
Omer Ronen <oronen@amazon.com>
2022-01-11 03:07:24 -0800

    improved stuff and added zsh customizations

--- install.sh
+++ install.sh
@@ -12,20 +12,35 @@ sleep 1
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 sleep 1
 
+echo "installing iTerm2..."
+brew install --cask iterm2
+sleep 2
 
 echo "Installing PL10K..."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sleep 1
 
-echo "Copying zshrc..."
+echo "Copying aliases..."
 cat aliases.txt > ~/.zshrc
+sleep 1
+
+echo "Installing zsh plugins..."
+cd ~/.oh-my.zsh/custom/plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+
+git clone https://github.com/jeffreytse/zsh-vi-mode \
+  $ZSH/custom/plugins/zsh-vi-mode
+
+cat zsh_ext.txt > ~/.zshrc
 
 echo "Installing pathogen..."
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 echo "Copying vimrc..."
-cat VimRC.txt >> ~/.vimrc
+cat vim_rc.txt >> ~/.vimrc
 sleep 1
 
 echo "Installing vim plugins..."
